2bdfcc88fe6912c6612368b25a8dd9b31964a4e5
tixuno <137496341+diepvantien@users.noreply.github.com>
2025-06-25 02:49:41 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service Worker để cache và tối ưu tốc độ
+const CACHE_NAME = 'pixelshare-v1';
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
